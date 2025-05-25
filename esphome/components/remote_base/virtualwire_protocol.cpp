deaa663c027ee49b454872db021d438a05756395
fightforlife <5511687+fightforlife@users.noreply.github.com>
2025-05-25 21:44:49 +0200

    original

--- esphome/components/remote_base/virtualwire_protocol.cpp
+++ esphome/components/remote_base/virtualwire_protocol.cpp
@@ -1,157 +1,243 @@
-#pragma once
-
-#include "esphome/core/component.h"
+#include "virtualwire_protocol.h"
+#include "esphome/core/log.h"
 #include "esphome/core/helpers.h"
-#include "remote_base.h"
-#include <array>
-#include <utility>
-#include <vector>
 
 namespace esphome {
 namespace remote_base {
 
-static const uint8_t MAX_LENGTH = 80;
-using tx_data_t = uint8_t;  // 6 bit encoded, transmitted LSB first!
+static const char *const TAG = "remote.virtualwire";
 
-class VirtualWireData {
- public:
-  // Make default
-  VirtualWireData() {
-    this->data_.resize(3);
-    std::fill(std::begin(this->data_), std::end(this->data_), 0);
-  }
-  // Make from initializer_list
-  VirtualWireData(std::initializer_list<uint8_t> data) {
-    size_t size = std::min(MAX_LENGTH, static_cast<uint8_t>(data.size()));
-    this->data_.resize(size);
-    std::copy_n(data.begin(), size, this->data_.begin());
-  }
-  // Make from vector
-  VirtualWireData(const std::vector<uint8_t> &data) {
-    size_t size = std::min(MAX_LENGTH, static_cast<uint8_t>(data.size()));
-    this->data_.resize(size);
-    std::copy_n(data.begin(), size, this->data_.begin());
-  }
-  // Default copy constructor
-  VirtualWireData(const VirtualWireData &) = default;
+static const std::vector<tx_data_t> VIRTUALWIRE_SYMBOLS = {0xd,  0xe,  0x13, 0x15, 0x16, 0x19, 0x1a, 0x1c,
+                                                           0x23, 0x25, 0x26, 0x29, 0x2a, 0x2c, 0x32, 0x34};
 
-  uint8_t *data() { return this->data_.data(); }
-  const uint8_t *data() const { return this->data_.data(); }
-  std::vector<uint8_t> get_raw_data() const { return this->data_; }
-  uint8_t size() const {
-    return std::min(static_cast<uint8_t>(3 + this->data_[0]), static_cast<uint8_t>(this->data_.size()));
-  }
-  bool is_valid() const;
-  void set_data(std::vector<uint8_t> data) {
-    uint8_t size = std::min(static_cast<uint8_t>(MAX_LENGTH - 3), static_cast<uint8_t>(data.size()));
-    this->data_.resize(size + 3);
-    this->data_[0] = size + 3;
-    std::copy_n(data.begin(), size, this->data_.begin() + 1);
-  }
-  void set_bit_length(uint32_t bit_length_us) { this->bit_length_us_ = bit_length_us; }
-  uint32_t get_bit_length() const { return this->bit_length_us_; }
-  std::vector<uint8_t> get_data() const {
-    std::vector<uint8_t> data(this->data_.begin() + 1, this->data_.begin() + 1 + get_data_size());
-    return data;
-  }
-  uint8_t get_data_size() const {
-    return std::min(static_cast<uint8_t>(MAX_LENGTH - 3), static_cast<uint8_t>(this->data_[0] - 3));
-  }
-  void finalize() {
-    uint16_t crc = this->calc_crc_();
-    this->data_[this->size() - 2] = crc & 0xff;
-    this->data_[this->size() - 1] = crc >> 8;
-  }
-  std::string to_string() const {
-    return format_hex_pretty(this->data_.data() + 1, this->get_data_size());
-    // return format_hex_pretty(this->data_.data(), this->size());
-  }
-  bool operator==(const VirtualWireData &rhs) const {
-    return std::equal(this->data_.begin(), this->data_.begin() + this->size(), rhs.data_.begin());
-  }
-  uint8_t &operator[](size_t idx) { return this->data_[idx]; }
-  const uint8_t &operator[](size_t idx) const { return this->data_[idx]; }
+static const std::vector<tx_data_t> VIRTUALWIRE_HEADER = {0x2a, 0x2a, 0x2a, 0x2a, 0x2a, 0x2a, 0x38, 0x2c};
 
- protected:
-  uint32_t bit_length_us_{500};  // 2000 bps
-  std::vector<uint8_t> data_;
+bool VirtualWireData::is_valid() const {
+  if (this->data_.size() < 4)
+    return false;
+  if (this->data_[0] < 4)
+    return false;
+  if (this->data_[0] > MAX_LENGTH)
+    return false;
+  if (this->data_.size() < this->data_[0])
+    return false;
+  uint16_t crc = this->calc_crc_();
+  return (this->data_[this->data_[0] - 2] == (crc & 0xff)) && (this->data_[this->data_[0] - 1] == (crc >> 8));
+}
 
-  // Calculate crc
-  uint16_t calc_crc_() const;
-};
+uint16_t VirtualWireData::calc_crc_() const {
+  if (this->data_.size() < 3)
+    return 0;
 
-class VirtualWireProtocol : public RemoteProtocol<VirtualWireData> {
- public:
-  void encode(RemoteTransmitData *dst, const VirtualWireData &src) override;
-  optional<VirtualWireData> decode(RemoteReceiveData src) override { return this->decode(src, 0); }
-  optional<VirtualWireData> decode(RemoteReceiveData src, uint32_t bit_length_us);
-  void dump(const VirtualWireData &data) override;
+  return crc16(this->data_.data(), this->size() - 2, 0xffff, 0x8408, false, true);
+}
 
- protected:
-  void encode_final_(RemoteTransmitData *dst, const std::vector<tx_data_t> &data, uint32_t bit_length_us) const;
-  int8_t peek_mark_(RemoteReceiveData &src, uint32_t bit_length_us);
-  int8_t peek_space_(RemoteReceiveData &src, uint32_t bit_length_us);
-  bool decode_symbol_6to4_(uint8_t encoded, uint8_t &data);
-  bool decode_4bit_(RemoteReceiveData &src, int8_t &remaining, bool &last_element, uint8_t &data,
-                    uint32_t bit_length_us);
-  bool decode_byte_(RemoteReceiveData &src, int8_t &remaining, bool &last_element, uint8_t &data,
-                    uint32_t bit_length_us);
-};
-
-class VirtualWireBinarySensor : public RemoteReceiverBinarySensorBase {
- public:
-  bool matches(RemoteReceiveData src) override {
-    auto data = VirtualWireProtocol().decode(src, this->bit_length_us_);
-    return data.has_value() && data.value() == this->data_;
+void VirtualWireProtocol::encode_final_(RemoteTransmitData *dst, const std::vector<tx_data_t> &data,
+                                        uint32_t bit_length_us) const {
+  bool prev_level = false;
+  uint32_t prev_length = 0;
+  for (const tx_data_t &d : data) {
+    for (tx_data_t mask = 1; mask & 0x3f; mask <<= 1) {
+      bool level = (d & mask);
+      if (level == prev_level) {
+        prev_length += bit_length_us;
+      } else {
+        if (prev_level) {
+          dst->mark(prev_length);
+        } else {
+          dst->space(prev_length);
+        }
+        prev_level = level;
+        prev_length = bit_length_us;
+      }
+    }
   }
-  void set_data(const std::vector<uint8_t> &data) { this->data_.set_data(data); }
-  void set_speed(uint16_t speed) {
-    if (!speed) {
-      this->bit_length_us_ = 0;
+  if (prev_level) {
+    dst->mark(prev_length);
+  } else {
+    dst->space(prev_length);
+  }
+}
+
+void VirtualWireProtocol::encode(RemoteTransmitData *dst, const VirtualWireData &src) {
+  dst->set_carrier_frequency(0);
+  dst->reserve(8 + 2 * src.size());
+
+  std::vector<tx_data_t> tx_buffer = VIRTUALWIRE_HEADER;
+  for (const uint8_t &d : src.get_raw_data()) {
+    tx_buffer.push_back(VIRTUALWIRE_SYMBOLS[d >> 4]);
+    tx_buffer.push_back(VIRTUALWIRE_SYMBOLS[d & 0xf]);
+  }
+  ESP_LOGVV(TAG, "TX: %s", format_hex_pretty(tx_buffer.data(), tx_buffer.size()).c_str());
+  this->encode_final_(dst, tx_buffer, src.get_bit_length());
+}
+
+int8_t VirtualWireProtocol::peek_mark_(RemoteReceiveData &src, uint32_t bit_length_us) {
+  if (src.peek_mark(bit_length_us))
+    return 1;
+  if (src.peek_mark(2 * bit_length_us))
+    return 2;
+  if (src.peek_mark(3 * bit_length_us))
+    return 3;
+  if (src.peek_mark(4 * bit_length_us))
+    return 4;
+  return 0;
+}
+
+int8_t VirtualWireProtocol::peek_space_(RemoteReceiveData &src, uint32_t bit_length_us) {
+  if (src.peek_space(bit_length_us))
+    return -1;
+  if (src.peek_space(2 * bit_length_us))
+    return -2;
+  if (src.peek_space(3 * bit_length_us))
+    return -3;
+  if (src.peek_space_at_least(4 * bit_length_us))
+    return -4;
+  return 0;
+}
+
+bool VirtualWireProtocol::decode_symbol_6to4_(uint8_t encoded, uint8_t &data) {
+  for (uint8_t i = (encoded >> 2) & 8, count = 8; count--; i++) {
+    if (encoded == VIRTUALWIRE_SYMBOLS[i]) {
+      data = i;
+      return true;
+    }
+  }
+  return false;
+}
+
+bool VirtualWireProtocol::decode_4bit_(RemoteReceiveData &src, int8_t &remaining, bool &last_element, uint8_t &data,
+                                       uint32_t bit_length_us) {
+  std::vector<int8_t> received_data = {remaining};
+  uint8_t num_elements = std::abs(remaining);
+  while (num_elements < 6) {
+    int8_t received_element;
+    if (last_element) {
+      received_element = this->peek_space_(src, bit_length_us);
+      src.advance();
+      ESP_LOGVV(TAG, "peek_space: %d", received_element);
+      num_elements += -received_element;
+      last_element = false;
     } else {
-      this->bit_length_us_ = 1000000 / static_cast<uint32_t>(speed);
+      received_element = this->peek_mark_(src, bit_length_us);
+      src.advance();
+      ESP_LOGVV(TAG, "peek_mark: %d", received_element);
+      num_elements += received_element;
+      last_element = true;
+    }
+    if (received_element == 0) {
+      ESP_LOGW(TAG, "Timing out of range");
+      return false;
+    }
+    received_data.push_back(received_element);
+  }
+  tx_data_t mask = 1;
+  tx_data_t encoded = 0;
+  for (const int8_t &element : received_data) {
+    ESP_LOGVV(TAG, "Got element: %d", element);
+    for (uint8_t i = std::abs(element); i; i--) {
+      if (element > 0) {
+        encoded |= mask;
+      }
+      mask <<= 1;
+      if (!(mask & 0x3f)) {
+        break;
+      }
     }
   }
-  void finalize() { this->data_.finalize(); }
-
- protected:
-  uint16_t bit_length_us_{0};
-  VirtualWireData data_;
-};
-
-using VirtualWireTrigger = RemoteReceiverTrigger<VirtualWireProtocol>;
-using VirtualWireDumper = RemoteReceiverDumper<VirtualWireProtocol>;
-
-template<typename... Ts> class VirtualWireAction : public RemoteTransmitterActionBase<Ts...> {
- public:
-  TEMPLATABLE_VALUE(uint16_t, speed)
-
-  void set_data_static(std::vector<uint8_t> data) { data_static_ = std::move(data); }
-  void set_data_template(std::function<std::vector<uint8_t>(Ts...)> func) {
-    this->data_func_ = func;
-    has_data_func_ = true;
+  if (!decode_symbol_6to4_(encoded, data)) {
+    ESP_LOGW(TAG, "6to4 code not found, encoded: %02x", encoded);
+    return false;
   }
-  void encode(RemoteTransmitData *dst, Ts... x) override {
-    VirtualWireData data;
-    if (has_data_func_) {
-      data.set_data(this->data_func_(x...));
-    } else {
-      data.set_data(this->data_static_);
+  if (num_elements > 6) {
+    num_elements -= 6;
+    remaining = received_data[received_data.size() - 1] > 0 ? num_elements : -num_elements;
+  } else {
+    remaining = 0;
+  }
+  ESP_LOGV(TAG, "4bit decode success, data: %01x, remaining: %d", data, remaining);
+  return true;
+}
+
+bool VirtualWireProtocol::decode_byte_(RemoteReceiveData &src, int8_t &remaining, bool &last_element, uint8_t &data,
+                                       uint32_t bit_length_us) {
+  uint8_t first, second;
+  if (!this->decode_4bit_(src, remaining, last_element, first, bit_length_us))
+    return false;
+  if (!this->decode_4bit_(src, remaining, last_element, second, bit_length_us))
+    return false;
+  data = ((first & 0x3f) << 4) | second;
+  return true;
+}
+
+optional<VirtualWireData> VirtualWireProtocol::decode(RemoteReceiveData src, uint32_t bit_length_us) {
+  if (src.size() < 12)
+    return {};
+
+  if (bit_length_us == 0) {
+    bool skip = false;
+    if (src.peek(0) < 0) {
+      skip = true;
+      src.advance(1);
     }
-    uint32_t speed_value = static_cast<uint32_t>(this->speed_.value(x...));
-    if (!speed_value) {
-      speed_value = 2000;
-    }
-    data.set_bit_length(1000000 / speed_value);
-    data.finalize();
-    VirtualWireProtocol().encode(dst, data);
+
+    // Speed detection
+    bit_length_us = (src.peek(0) - src.peek(1) + src.peek(2) - src.peek(3) + src.peek(4) - src.peek(5)) / 6;
+    ESP_LOGI(TAG, "Trying to detect speed, bit_length_us: %i, skip: %i", bit_length_us, skip);
   }
 
- protected:
-  std::function<std::vector<uint8_t>(Ts...)> data_func_{};
-  std::vector<uint8_t> data_static_{};
-  bool has_data_func_{false};
-};
+  if (src.expect_item(bit_length_us, bit_length_us) && src.expect_item(bit_length_us, bit_length_us) &&
+      src.expect_item(bit_length_us, bit_length_us)) {
+    ESP_LOGI(TAG, "Header start");
+
+    while (src.peek_item(bit_length_us, bit_length_us)) {
+      src.advance(2);
+    }
+
+    if (!(src.expect_item(bit_length_us, 3 * bit_length_us) && src.expect_item(3 * bit_length_us, 2 * bit_length_us) &&
+          src.expect_item(2 * bit_length_us, bit_length_us))) {
+      ESP_LOGW(TAG, "Header error");
+      return {};
+    }
+
+    int8_t mark = this->peek_mark_(src, bit_length_us);
+    src.advance();
+    if (mark == 0)
+      return {};
+
+    ESP_LOGI(TAG, "Header complete");
+    int8_t remaining = mark - 1;
+    bool last_element = true;
+    uint8_t expected_length = 4;
+    uint8_t data;
+
+    if (!this->decode_byte_(src, remaining, last_element, expected_length, bit_length_us))
+      return {};
+    std::vector<uint8_t> out_data = {expected_length};
+    expected_length--;
+    for (; expected_length; expected_length--) {
+      if (!this->decode_byte_(src, remaining, last_element, data, bit_length_us)) {
+        ESP_LOGW(TAG, "Error decoding byte");
+        return {};
+      }
+      ESP_LOGI(TAG, "Decoded byte %02x", data);
+      out_data.push_back(data);
+    }
+    VirtualWireData out = VirtualWireData(out_data);
+    out.set_bit_length(bit_length_us);
+
+    if (!out.is_valid()) {
+      ESP_LOGW(TAG, "Checksum error");
+      return {};
+    }
+    return out;
+  }
+  return {};
+}
+
+void VirtualWireProtocol::dump(const VirtualWireData &data) {
+  ESP_LOGD(TAG, "Received VirtualWire: %s", data.to_string().c_str());
+}
 
 }  // namespace remote_base
 }  // namespace esphome
